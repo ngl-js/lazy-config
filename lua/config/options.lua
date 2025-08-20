516d328348560267bb37159dda5552387d67b8ef
ngl-js <joshua.dq@outlook.com>
2025-08-20 14:11:23 -0500

    Config for linux terminal, plugins upgrades

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,19 +1,22 @@
--- Check if 'pwsh' is executable and set the shell accordingly
-if vim.fn.executable("pwsh") == 1 then
-  vim.o.shell = "pwsh"
-else
-  vim.o.shell = "powershell"
+-- Set terminal on Windows
+if not vim.fn.executable("bash") then
+  -- Check if 'pwsh' is executable and set the shell accordingly
+  if vim.fn.executable("pwsh") == 1 then
+    vim.o.shell = "pwsh"
+  else
+    vim.o.shell = "powershell"
+  end
+  -- Setting shell command flags
+  vim.o.shellcmdflag =
+    "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';"
+  -- Setting shell redirection
+  vim.o.shellredir = '2>&1 | %{ "$_" } | Out-File %s; exit $LastExitCode'
+  -- Setting shell pipe
+  vim.o.shellpipe = '2>&1 | %{ "$_" } | Tee-Object %s; exit $LastExitCode'
+  -- Setting shell quote options
+  vim.o.shellquote = ""
+  vim.o.shellxquote = ""
 end
--- Setting shell command flags
-vim.o.shellcmdflag =
-  "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';"
--- Setting shell redirection
-vim.o.shellredir = '2>&1 | %{ "$_" } | Out-File %s; exit $LastExitCode'
--- Setting shell pipe
-vim.o.shellpipe = '2>&1 | %{ "$_" } | Tee-Object %s; exit $LastExitCode'
--- Setting shell quote options
-vim.o.shellquote = ""
-vim.o.shellxquote = ""
 
 vim.opt.winbar = "%=%m %f"
 vim.opt.guicursor = ""
